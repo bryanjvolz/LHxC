55ff9e6b78cf8b92d61e4f6e6fefde1bc8f45a49
Bryan Volz <bryan@typocracy.net>
2023-11-22 12:12:44 -0500

    Update latest from dev env

--- index.php
+++ index.php
@@ -9,14 +9,14 @@ get_header(); ?>
 	<div id="content" class="narrowcolumn WHAT?">
 
 	<?php query_posts($query_string . '&cat=-249'); ?>
- <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+	 <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
 			<div <?php post_class() ?> id="post-<?php the_ID(); ?>">
 				<h2><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
 				<small><?php the_time('F jS, Y') ?></small>
 
-				<div class="entry">
-				<?php
+				<div class="entry cf">
+					<?php
 					  	  $key = 'HomePageBanner';
 					  	  $themeta = get_post_meta($post->ID, $key, TRUE);
 					  	  if($themeta != '') { ?>
@@ -25,12 +25,7 @@ get_header(); ?>
 					  	    $my_custom_field = $custom_fields['HomePageBanner'];
 					  	    foreach ( $my_custom_field as $key => $value )
 					  	    echo $value; ?> " alt="<?php the_title(); ?>" class="entryBanner"></a>
-					  	  <?php }
-
-					  	  else {
-					  	  echo '';
-					  	  }
-					  	  ?>
+					  	  <?php } ?>
 
 					<?php the_content('Read the rest of this entry &raquo;'); ?>
 				</div>
@@ -44,11 +39,9 @@ get_header(); ?>
 
 
 	<?php else : ?>
-
 		<h2 class="center">Not Found</h2>
 		<p class="center">Sorry, but you are looking for something that isn't here.</p>
 		<?php get_search_form(); ?>
-
 	<?php endif; ?>
 
 	</div>
--- single.php
+++ single.php
@@ -20,7 +20,7 @@ get_header();
 		<div <?php post_class() ?> id="post-<?php the_ID(); ?>">
 			<h2><?php the_title(); ?></h2>
 
-			<div class="entry">
+			<div class="entry cf">
 
 			<?php
 			if( get_post_meta($post->ID, 'BannerFullSizeLink', true) ) { ?>
