7ecd74b1e97a688ee3b6ccbb70a2f962cadf743d
Bryan Volz <bryan@typocracy.net>
2024-03-04 13:17:23 -0500

    Linting fix for posts functions

--- functions/posts.php
+++ functions/posts.php
@@ -44,7 +44,7 @@ add_action( 'save_post', 'lhxc_default_featured_image' );
 function set_default_featured_image_for_existing_posts() {
 	/** Replace 'your_default_image_url' with the URL of your default featured image */
 	// $default_image_url = 'https://www.louisvillehardcore.com/wp-content/uploads/2024/02/52986395372_1f08e95b33_c.jpg';
-  $default_image_url = get_default_feature_image();
+	$default_image_url = get_default_feature_image();
 
 	/** Get the attachment ID of the default image */
 	$default_image_id = attachment_url_to_postid( $default_image_url );
