b371b9e232d86de9c79f68bc435399bb285b45bb
Bryan Volz <bryan@typocracy.net>
2024-03-04 13:19:43 -0500

    Linting update, remove extra comments to not trigger lint counter

--- functions/posts.php
+++ functions/posts.php
@@ -5,9 +5,8 @@
  * @package LHXC
  */
 
-/** Add thumbnails */
+/** Add thumbnails - Paste this where you wanna use it - the_post_thumbnail(); */
 add_theme_support( 'post-thumbnails' );
-/** Paste this where you wanna use it - the_post_thumbnail(); */
 
 /**
  * Add odd or even post classes to the post for zebra striping.
@@ -30,9 +29,7 @@ $current_class = 'odd';
 function lhxc_default_featured_image() {
 	if ( ! has_post_thumbnail() ) {
 		$default_image_url = get_default_feature_image();
-		/** Get the attachment ID of the default image */
-		$default_image_id = attachment_url_to_postid( $default_image_url );
-		/** Set the default image as the featured image */
+		$default_image_id  = attachment_url_to_postid( $default_image_url );
 		set_post_thumbnail( get_the_ID(), $default_image_id );
 	}
 }
@@ -42,8 +39,6 @@ add_action( 'save_post', 'lhxc_default_featured_image' );
  * Set a default feature image
  */
 function set_default_featured_image_for_existing_posts() {
-	/** Replace 'your_default_image_url' with the URL of your default featured image */
-	// $default_image_url = 'https://www.louisvillehardcore.com/wp-content/uploads/2024/02/52986395372_1f08e95b33_c.jpg';
 	$default_image_url = get_default_feature_image();
 
 	/** Get the attachment ID of the default image */
@@ -51,15 +46,14 @@ function set_default_featured_image_for_existing_posts() {
 
 	/** Query to retrieve posts and pages that do not have a featured image set */
 	$args = array(
-		'post_type'              => array( 'post', 'page' ),
-		/** You can include additional post types if needed */
-					'meta_query' => array(
-						array(
-							'key'     => '_thumbnail_id',
-							'compare' => 'NOT EXISTS',
-						),
-					),
-		'posts_per_page'         => -1,
+		'post_type'      => array( 'post', 'page' ),
+		'meta_query'     => array(
+			array(
+				'key'     => '_thumbnail_id',
+				'compare' => 'NOT EXISTS',
+			),
+		),
+		'posts_per_page' => -1,
 	);
 
 	$query = new WP_Query( $args );
